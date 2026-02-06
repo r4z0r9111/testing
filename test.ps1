7bcc5f1543c95d65511a0b10e0714a95cb075a2d
r4z0r9111 <83854003+r4z0r9111@users.noreply.github.com>
2026-02-06 14:26:26 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,3 +4,10 @@ Connect-ExchangeOnline -UserPrincipalName your-email@domain.com
 # Get connection info (EXOv3 specific)
 $Connection = Get-ConnectionInformation
 $Connection | Format-List
+
+# EXOv3 uses Get-ManagementRoleAssignment with ExchangeOnlineManagement module
+Get-ManagementRoleAssignment -GetEffectiveUsers | 
+    Where-Object { $_.EffectiveUserName -eq (Get-ConnectionInformation).UserPrincipalName } |
+    Select-Object Role, RoleAssigneeType, AssignmentMethod, CustomRecipientWriteScope |
+    Sort-Object Role -Unique |
+    Format-Table -AutoSize
