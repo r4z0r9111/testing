e6d3ee60d961138246205a4670a68be03e0e0243
r4z0r9111 <83854003+r4z0r9111@users.noreply.github.com>
2026-02-25 01:35:03 -0500

    Update census2.ps1

--- census2.ps1
+++ census2.ps1
@@ -4,14 +4,18 @@ param(
 
 Connect-MgGraph -Scopes User.Read.All,AuditLog.Read.All,Directory.Read.All -NoWelcome | Out-Null
 
-$users = Get-MgUser -Filter "department eq '$Department'" -All -Property displayName,mail,accountEnabled,SignInActivity
+$users = Get-MgUser -Filter "department eq '$Department'" -All -Property mail,displayName,accountEnabled,signInActivity
 
 if($users){
-    $users | Select-Object displayName,
-                          mail,
-                          @{N='AccountStatus';E={if($_.accountEnabled){'Active'}else{'Disabled'}}},
-                          @{N='LastSignIn';E={if($_.SignInActivity.LastSignInDateTime){([datetime]$_.SignInActivity.LastSignInDateTime).ToString('yyyy-MM-dd')}else{'Never'}}} |
-             Sort-Object displayName
+    $users | ForEach-Object {
+        $lastSignIn = if($_.signInActivity.lastSignInDateTime){$_.signInActivity.lastSignInDateTime}else{"Never"}
+        [PSCustomObject]@{
+            Name = $_.displayName
+            Email = $_.mail
+            Status = if($_.accountEnabled){"Active"}else{"Disabled"}
+            LastSignIn = $lastSignIn
+        }
+    } | Sort-Object Name
 } else {
     Write-Host "No users found in department: $Department"
 }
