9daab5b4dde2fb3b8926d6df857a287f26db77c8
r4z0r9111 <83854003+r4z0r9111@users.noreply.github.com>
2026-02-25 01:30:08 -0500

    Update census2.ps1

--- census2.ps1
+++ census2.ps1
@@ -2,12 +2,16 @@ param(
     [Parameter(Mandatory=$true)]$Department
 )
 
-Connect-MgGraph -Scopes User.Read.All,Directory.Read.All -NoWelcome | Out-Null
+Connect-MgGraph -Scopes User.Read.All,AuditLog.Read.All,Directory.Read.All -NoWelcome | Out-Null
 
-$users = Get-MgUser -Filter "department eq '$Department'" -All -Property mail,displayName
+$users = Get-MgUser -Filter "department eq '$Department'" -All -Property displayName,mail,accountEnabled,SignInActivity
 
 if($users){
-    $users | Select-Object displayName,mail | Sort-Object displayName
+    $users | Select-Object displayName,
+                          mail,
+                          @{N='AccountStatus';E={if($_.accountEnabled){'Active'}else{'Disabled'}}},
+                          @{N='LastSignIn';E={if($_.SignInActivity.LastSignInDateTime){([datetime]$_.SignInActivity.LastSignInDateTime).ToString('yyyy-MM-dd')}else{'Never'}}}
+                          | Sort-Object displayName
 } else {
     Write-Host "No users found in department: $Department"
 }
