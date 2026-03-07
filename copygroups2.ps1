6991be663b52880da18bd95f27ec11e2231317a0
r4z0r9111 <83854003+r4z0r9111@users.noreply.github.com>
2026-03-07 04:46:14 -0500

    Update copygroups2.ps1

--- copygroups2.ps1
+++ copygroups2.ps1
@@ -1,20 +1,25 @@
+Import-Module Microsoft.Graph.Users
+Import-Module Microsoft.Graph.Groups
+Import-Module Microsoft.Graph.Authentication
+
 Connect-MgGraph -Scopes User.Read.All,AuditLog.Read.All,Directory.Read.All -NoWelcome | Out-Null
 
 $groups = Get-MgUserMemberOf -UserId (Get-MgUser -UserId "").Id -All |
-   Where-Object { $_.AdditionalProperties.'@odata.type' -eq '#microsoft.graph.group' -and 
-                  $_.AdditionalProperties.groupTypes -notcontains 'DynamicMembership' } |
-   Select-Object Id, @{N='MailEnabled';E={$_.AdditionalProperties.mailEnabled}}
+    Where-Object { $_.AdditionalProperties.'@odata.type' -eq '#microsoft.graph.group' -and 
+                   $_.AdditionalProperties.groupTypes -notcontains 'DynamicMembership' } |
+    Select-Object Id, @{N='MailEnabled';E={$_.AdditionalProperties.mailEnabled}}
 
 $targetID = (Get-MgUser -UserId "").Id
 
+Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline -ShowBanner:$false
 
 $groups | ForEach-Object { 
-   if ($_.MailEnabled) {
-       Add-DistributionGroupMember -Identity $_.Id -Member $targetID -BypassSecurityGroupManagerCheck -Confirm:$false
-   } else {
-       New-MgGroupMember -GroupId $_.Id -DirectoryObjectId $targetID
-   }
+    if ($_.MailEnabled) {
+        Add-DistributionGroupMember -Identity $_.Id -Member $targetID -BypassSecurityGroupManagerCheck -Confirm:$false
+    } else {
+        New-MgGroupMember -GroupId $_.Id -DirectoryObjectId $targetID
+    }
 }
 
 Disconnect-ExchangeOnline -Confirm:$false | Out-Null
