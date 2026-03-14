b23eb5559a86ca1583214fbc1acc9a9e65104d81
r4z0r9111 <83854003+r4z0r9111@users.noreply.github.com>
2026-03-14 12:01:48 -0500

    Update test4.ps1

--- test4.ps1
+++ test4.ps1
@@ -1,1 +1,13 @@
 Get-MgGroup -Top 1 | ForEach-Object { $g=$_; Write-Host "`nGroup: $($g.DisplayName)" -ForegroundColor Green; Get-MgGroupMember -GroupId $g.Id | Select-Object -ExpandProperty AdditionalProperties | Select-Object displayName,mail }
+
+Get-MgGroup -Top 1 | ForEach-Object {
+    $g = $_
+    Write-Host "`nGroup: $($g.DisplayName)" -ForegroundColor Green
+    Get-MgGroupMember -GroupId $g.Id | ForEach-Object {
+        $props = $_.AdditionalProperties
+        [PSCustomObject]@{
+            DisplayName = $props['displayName']
+            Mail        = $props['mail']
+        }
+    }
+}
